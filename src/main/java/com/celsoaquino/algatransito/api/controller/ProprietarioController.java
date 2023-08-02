52911b967a38690b604758ce71a864adb3d54173
celsoaquino <celso.aquino@ymail.com>
2023-08-02 18:36:39 -0300

    Implementing a Collection Resource

---
+++ src/main/java/com/celsoaquino/algatransito/api/controller/ProprietarioController.java
@@ -0,0 +1,19 @@
+package com.celsoaquino.algatransito.api.controller;
+
+import com.celsoaquino.algatransito.domain.model.Proprietario;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ProprietarioController {
+
+    @GetMapping("/proprietarios")
+    public List<Proprietario> list() {
+        return List.of(
+                new Proprietario(1L, "Celso", "email", "156465"),
+                new Proprietario(2L, "Joao", "adsad", "5465465"));
+    }
+}
