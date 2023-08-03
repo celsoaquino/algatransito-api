f32ade7bc9820693324055c174685b8da15eb92b
celsoaquino <celso.aquino@ymail.com>
2023-08-03 18:45:48 -0300

    Boosting Productivity with DevTools

--- src/main/java/com/celsoaquino/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/celsoaquino/algatransito/api/controller/ProprietarioController.java
@@ -9,7 +9,7 @@ import java.util.List;
 @RestController
 public class ProprietarioController {
 
-    @GetMapping("/proprietarios")
+    @GetMapping("/proprietarios2")
     public List<Proprietario> list() {
         return List.of(
                 new Proprietario(1L, "Celso", "email", "156465"),
