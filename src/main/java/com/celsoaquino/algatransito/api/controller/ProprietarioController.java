fba3ff99a1641f2d100d577e3c586065f475e3f9
celsoaquino <celso.aquino@ymail.com>
2023-08-16 16:47:42 -0300

    Implementing Query Methods in the repository

--- src/main/java/com/celsoaquino/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/celsoaquino/algatransito/api/controller/ProprietarioController.java
@@ -3,7 +3,6 @@ package com.celsoaquino.algatransito.api.controller;
 import com.celsoaquino.algatransito.domain.model.Proprietario;
 import com.celsoaquino.algatransito.domain.repository.ProprietarioRespository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/celsoaquino/algatransito/domain/repository/ProprietarioRespository.java
+++ src/main/java/com/celsoaquino/algatransito/domain/repository/ProprietarioRespository.java
@@ -4,6 +4,10 @@ import com.celsoaquino.algatransito.domain.model.Proprietario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProprietarioRespository extends JpaRepository<Proprietario, Long> {
+
+    List<Proprietario> findByName(String name);
 }
