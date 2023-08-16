91921fb9d432c78f436a9c3415b0ea869c64e278
celsoaquino <celso.aquino@ymail.com>
2023-08-16 16:39:13 -0300

    Injecting and using the SDJ repository

--- src/main/java/com/celsoaquino/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/celsoaquino/algatransito/api/controller/ProprietarioController.java
@@ -1,22 +1,22 @@
 package com.celsoaquino.algatransito.api.controller;
 
 import com.celsoaquino.algatransito.domain.model.Proprietario;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
+import com.celsoaquino.algatransito.domain.repository.ProprietarioRespository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 public class ProprietarioController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    private final ProprietarioRespository repository;
 
     @GetMapping("/proprietarios")
     public List<Proprietario> list() {
-        return manager.createQuery("from Proprietario", Proprietario.class).getResultList();
+        return repository.findAll();
     }
 }
