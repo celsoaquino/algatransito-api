28fd3811c1f1e3b2bddcc6c1cb5c8751ebba639f
celsoaquino <celso.aquino@ymail.com>
2023-08-17 00:09:50 -0300

    Implementing resource fetch endpoint

--- src/main/java/com/celsoaquino/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/celsoaquino/algatransito/api/controller/ProprietarioController.java
@@ -3,19 +3,28 @@ package com.celsoaquino.algatransito.api.controller;
 import com.celsoaquino.algatransito.domain.model.Proprietario;
 import com.celsoaquino.algatransito.domain.repository.ProprietarioRespository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
 
     private final ProprietarioRespository repository;
 
-    @GetMapping("/proprietarios")
+    @GetMapping
     public List<Proprietario> list() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> findById(@PathVariable Long proprietarioId) {
+        return repository.findById(proprietarioId).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 }
