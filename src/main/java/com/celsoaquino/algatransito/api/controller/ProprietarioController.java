5e7ab91c75e04a297b2722e1e8e77a12015007bf
celsoaquino <celso.aquino@ymail.com>
2023-08-17 00:19:37 -0300

    Implementing feature inclusion endpoint

--- src/main/java/com/celsoaquino/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/celsoaquino/algatransito/api/controller/ProprietarioController.java
@@ -3,11 +3,9 @@ package com.celsoaquino.algatransito.api.controller;
 import com.celsoaquino.algatransito.domain.model.Proprietario;
 import com.celsoaquino.algatransito.domain.repository.ProprietarioRespository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,11 @@ public class ProprietarioController {
     @GetMapping("/{proprietarioId}")
     public ResponseEntity<Proprietario> findById(@PathVariable Long proprietarioId) {
         return repository.findById(proprietarioId).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario create(@RequestBody Proprietario proprietario) {
+        return repository.save(proprietario);
     }
 }
