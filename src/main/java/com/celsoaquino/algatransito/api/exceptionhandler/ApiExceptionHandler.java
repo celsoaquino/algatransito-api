b2dc6ce31eaf5c07df103e446357df6af4fb7c81
celsoaquino <celso.aquino@ymail.com>
2023-08-19 19:29:04 -0300

    Customizing ProblemDetail information

--- src/main/java/com/celsoaquino/algatransito/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/celsoaquino/algatransito/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,10 +1,17 @@
 package com.celsoaquino.algatransito.api.exceptionhandler;
 
 import com.celsoaquino.algatransito.domain.exception.NegocioException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
 
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -12,5 +19,19 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<String> captura(NegocioException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos são inválidos");
+        problemDetail.setType(URI.create("https://algatransito.com/erros/campos-invalidos"));
+
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+
     }
 }
