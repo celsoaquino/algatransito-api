c48ee9806bcb2e65dddcf0b11e77b61195bb5864
celsoaquino <celso.aquino@ymail.com>
2023-08-20 15:58:50 -0300

    Creating the Vehicle Resource Representation Model

--- src/main/java/com/celsoaquino/algatransito/api/controller/VeiculoController.java
+++ src/main/java/com/celsoaquino/algatransito/api/controller/VeiculoController.java
@@ -1,5 +1,6 @@
 package com.celsoaquino.algatransito.api.controller;
 
+import com.celsoaquino.algatransito.api.model.VeiculoModel;
 import com.celsoaquino.algatransito.domain.exception.NegocioException;
 import com.celsoaquino.algatransito.domain.model.Veiculo;
 import com.celsoaquino.algatransito.domain.repository.VeiculoRepository;
@@ -26,8 +27,21 @@ public class VeiculoController {
     }
 
     @GetMapping("/{veiculoId}")
-    public ResponseEntity<Veiculo> findById(@PathVariable Long veiculoId) {
+    public ResponseEntity<VeiculoModel> findById(@PathVariable Long veiculoId) {
         return veiculoRepository.findById(veiculoId)
+                .map(veiculo -> {
+                    var veiculoModel = new VeiculoModel();
+                    veiculoModel.setId(veiculo.getId());
+                    veiculoModel.setNomeProprietario(veiculo.getProprietario().getName());
+                    veiculoModel.setMarca(veiculo.getMarca());
+                    veiculoModel.setModelo(veiculo.getModelo());
+                    veiculoModel.setPlaca(veiculo.getPlaca());
+                    veiculoModel.setStatus(veiculo.getStatus());
+                    veiculoModel.setDataCadastro(veiculo.getDataCadastro());
+                    veiculoModel.setDataAprensao(veiculo.getDataAprensao());
+
+                    return veiculoModel;
+                })
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
---
+++ src/main/java/com/celsoaquino/algatransito/api/model/VeiculoModel.java
@@ -0,0 +1,22 @@
+package com.celsoaquino.algatransito.api.model;
+
+import com.celsoaquino.algatransito.domain.model.StatusVeiculo;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+public class VeiculoModel {
+
+    private Long id;
+    private String marca;
+    private String modelo;
+    private String placa;
+    private StatusVeiculo status;
+    private OffsetDateTime dataCadastro;
+    private OffsetDateTime dataAprensao;
+    private String nomeProprietario;
+
+}
