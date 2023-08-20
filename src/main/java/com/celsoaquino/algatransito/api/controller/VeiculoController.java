9ac55b777ba8793b9770c02e9b0e48a0b737937e
celsoaquino <celso.aquino@ymail.com>
2023-08-20 16:43:07 -0300

    Implementing Representation Model assembler

--- src/main/java/com/celsoaquino/algatransito/api/controller/VeiculoController.java
+++ src/main/java/com/celsoaquino/algatransito/api/controller/VeiculoController.java
@@ -1,13 +1,12 @@
 package com.celsoaquino.algatransito.api.controller;
 
+import com.celsoaquino.algatransito.api.assembler.VeiculoAssembler;
 import com.celsoaquino.algatransito.api.model.VeiculoModel;
-import com.celsoaquino.algatransito.domain.exception.NegocioException;
 import com.celsoaquino.algatransito.domain.model.Veiculo;
 import com.celsoaquino.algatransito.domain.repository.VeiculoRepository;
 import com.celsoaquino.algatransito.domain.service.RegistroVeiculoService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,24 +20,24 @@ public class VeiculoController {
 
     private final VeiculoRepository veiculoRepository;
     private final RegistroVeiculoService registroVeiculoService;
-    private final ModelMapper modelMapper;
+    private final VeiculoAssembler veiculoAssembler;
 
     @GetMapping
-    public List<Veiculo> list() {
-        return veiculoRepository.findAll();
+    public List<VeiculoModel> list() {
+        return veiculoAssembler.toCollectionModel(veiculoRepository.findAll());
     }
 
     @GetMapping("/{veiculoId}")
     public ResponseEntity<VeiculoModel> findById(@PathVariable Long veiculoId) {
         return veiculoRepository.findById(veiculoId)
-                .map(veiculo -> modelMapper.map(veiculo, VeiculoModel.class))
+                .map(veiculoAssembler::toModel)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Veiculo cadastrar(@Valid @RequestBody Veiculo veiculo) {
-        return registroVeiculoService.cadastrar(veiculo);
+    public VeiculoModel cadastrar(@Valid @RequestBody Veiculo veiculo) {
+        return veiculoAssembler.toModel(registroVeiculoService.cadastrar(veiculo));
     }
 }
